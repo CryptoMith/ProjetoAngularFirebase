f80dde232c67b794be243765691e51431721ed81
Matheus Minzon <125033723+CryptoMith@users.noreply.github.com>
2023-08-25 13:26:30 +0000

    implementação do banco de dados

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,13 @@
 
 export const environment = {
   firebaseConfig: {
-    apiKey: "AIzaSyAK2V1BSfl7BI6LiV0ei-YeyyWK_zAnwUA",
-    authDomain: "projetoexemplo-e640c.firebaseapp.com",
-    projectId: "projetoexemplo-e640c",
-    storageBucket: "projetoexemplo-e640c.appspot.com",
-    messagingSenderId: "782096819711",
-    appId: "1:782096819711:web:f72b6236e92b24219973ca",
-    measurementId: "G-520YLH2FX3"
+    apiKey: "AIzaSyDi9eeLdIGgQerXFdXi3NZg_tKDI79LD7I",
+    authDomain: "projetoexemplo-bbd34.firebaseapp.com",
+    projectId: "projetoexemplo-bbd34",
+    storageBucket: "projetoexemplo-bbd34.appspot.com",
+    messagingSenderId: "102071668798",
+    appId: "1:102071668798:web:29ebf365f05ccbe68b816f",
+    measurementId: "G-K73JWLSBLC"
   },
   production: false
 };
