396baad368a3c34fb2b008cd16f1de9b5783cb74
Matheus Minzon <125033723+CryptoMith@users.noreply.github.com>
2023-09-18 14:07:35 +0000

    Criação da página cadastro e implementação do js

---
+++ src/app/cadastro/cadastro.page.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthenticateService } from '../services/auth.service';
+import { CrudService } from '../services/crud.service';
+import { Storage, getDownloadURL, ref, uploadBytesResumable } from '@angular/fire/storage';
+import { MessageService } from '../services/message.service';
+
+
+@Component({
+  selector: 'app-cadastro',
+  templateUrl: './cadastro.page.html',
+  styleUrls: ['./cadastro.page.scss'],
+})
+export class CadastroPage implements OnInit {
+
+  constructor(
+    public _authenticate: AuthenticateService,
+    private _crudService: CrudService,
+    public storage: Storage,
+    private _message: MessageService
+  ) { }
+
+  ngOnInit() {
+  }
+  criarConta(dados: any){
+    this._authenticate.register(dados.email, dados.password)
+  }
+}
